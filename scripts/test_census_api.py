b2a79693f3a241f0001963cae13049361a89cc5e
Kostas Alexandridis <ktalexan@outlook.com>
2026-01-22 12:18:09 -0800

    feat: add script to test Census API data fetching functionality

--- scripts/test_census_api.py
+++ scripts/test_census_api.py
@@ -7,10 +7,10 @@ load_dotenv()
 from scripts.CensusAPI import ACSAPI
 
 # Initialize the ACSAPI class
-acs = ACSAPI(year=2022, dataset='acs/acs5')
+acs = ACSAPI(year = 2022, dataset = 'acs/acs5')
 
 # Fetch data
-variables = ['B01001_001E', 'B02001_002E', 'B02001_003E']
-geo = 'tract:*'
+variables = ["B01001_001E"]
+geo = "tract:*"
 data = acs.fetch_data(variables, geo)
 print(data.head())
